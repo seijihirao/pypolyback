ac1909b4340cc80f0f1947ac37a9776e99eb69e9
Seiji Hirao <rodrigo.seiji@clubedaentrega.com.br>
2016-06-05 03:08:05 -0300

    Added: apiobject module.

---
+++ api/apiobject.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+from api import config, log
+
+class _ApiObject(object):
+    """
+    Api properties that will be avaliable on endpoints
+    
+    Properties:
+        config: current config object
+        debug: function to log message
+        error: function to log error
+        (will be added endpoint required util modules)
+    """
+    
+    config = config.value
+    debug = log.debug
+    error = log.error
+
+def mount():
+    return _ApiObject()
