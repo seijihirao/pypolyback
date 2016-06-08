8b74655c7954bc7153016e9016a0258c8a99fb22
Seiji Hirao <rodrigo.seiji@clubedaentrega.com.br>
2016-06-08 01:58:28 -0300

    Comas........

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     version='0.5.3',
 
     description='A simple back-end rest framework in python using twisted and cyclone',
-    long_description='Github: https://github.com/seijihirao/pypolyback'
+    long_description='Github: https://github.com/seijihirao/pypolyback',
 
     # The project's main homepage.
     url='https://github.com/seijihirao/pypolyback',
